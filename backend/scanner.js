a123ced5c1eed2de06f88ec883ebd8235587b043
Slimenian <slimenian@gmail.com>
2021-03-27 15:44:36 +0100

    Dodajanje scanna za OS IP-ja

--- backend/scanner.js
+++ backend/scanner.js
@@ -27,3 +27,16 @@ stealthscan.on('error', function(error) {
 })
 
 stealthscan.startScan();
+
+
+// OS and port scan ti pove se verzijo OS/porta kar lahko pomaga ker assumas da starejsi kot je version lazje je hackable ker ni uptodate version.
+var osandports = new nmap.OsAndPortScan('192.168.1.4');
+ 
+osandports.on('complete',function(data){
+  console.log(data);
+});
+osandports.on('error', function(error){
+  console.log(error);
+});
+ 
+osandports.startScan();

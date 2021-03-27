1b060045107330ed539e06e8b37ca2e31a1519fd
Robi <34911342+robiworks@users.noreply.github.com>
2021-03-27 15:53:08 +0100

    Rewrite createResponse function

--- backend/scanner.js
+++ backend/scanner.js
@@ -22,7 +22,7 @@ var stealthscan = new nmap.NmapScan('fri.uni-lj.si', '-sS');
 
 stealthscan.on('complete', function(data) {
     //console.log(data);
-    var jsonResponse = createResponse(data[0].hostname, data[0].ip, data[0].openPorts);
+    var jsonResponse = createResponse(data);
     console.log(jsonResponse);
 })
 
@@ -34,21 +34,22 @@ stealthscan.startScan();
 
 
 // OS and port scan ti pove se verzijo OS/porta kar lahko pomaga ker assumas da starejsi kot je version lazje je hackable ker ni uptodate version.
-var osandports = new nmap.OsAndPortScan('192.168.1.4');
+var osandports = new nmap.OsAndPortScan('fri.uni-lj.si');
  
 osandports.on('complete',function(data){
-  console.log(data);
+    console.log(data);
 });
+
 osandports.on('error', function(error){
-  console.log(error);
+    console.log(error);
 });
  
 osandports.startScan();
 
-function createResponse(hostname, ip, ports) {
+function createResponse(data) {
     var jsonResponse = new Object();
-    jsonResponse.hostname = hostname;
-    jsonResponse.ip = ip;
-    jsonResponse.ports = ports;
+    jsonResponse.hostname = data[0].hostname;
+    jsonResponse.ip = data[0].ip;
+    jsonResponse.ports = data[0].openPorts;
     return jsonResponse;
 }

d54364d1a2de0877f8e0cf84aeca53be50a0fd54
Robi <34911342+robiworks@users.noreply.github.com>
2021-03-28 12:47:09 +0200

    Production

---
+++ scanner.js
@@ -0,0 +1,63 @@
+const nmap = require('node-nmap');
+const mime = require("mime-types");
+const formidable = require("formidable");
+const http = require("http");
+const fs = require("fs-extra");
+const util = require("util");
+const path = require("path");
+const find = require('local-devices');
+const express = require("express");
+
+
+
+
+nmap.nmapLocation = "nmap";
+
+if (!process.env.PORT) process.env.PORT = 8080;
+
+const streznik = express();
+
+streznik.use(express.static("public"));
+
+/*streznik.get("/", (req,odgovor) => {
+    //let k = fs.readFileSync(path.resolve(__dirname, "../spletna-stran/index.html"));
+    odgovor.sendFile(path.resolve(__dirname, "index.html"));
+});*/
+
+streznik.get("/api", (req, odgovor) => {
+    doAPIThing(odgovor);
+});
+
+streznik.get("/*", (req, odgovor) => {
+    odgovor.sendFile(path.resolve(__dirname, req.originalUrl));
+});
+
+streznik.listen(process.env.PORT, () => {
+    console.log("Strežnik je pognan!");
+});
+
+async function doAPIThing(odgovor) {
+    let detected;
+    detected = await find();
+    var ob = [];
+    for (var i = 0; i < detected.length; i++) {
+        ob[i] = detected[i].ip;
+    }
+    let h = await detailedScan(ob, odgovor);
+    return h;
+
+}
+
+
+async function detailedScan(host, odgovor) {
+    var osandports = new nmap.NmapScan(host, '-sS --max-retries 1 -T5');
+    var k = osandports.on('complete', function (data) {
+        odgovor.send(data)
+    });
+    osandports.on('error', function (error) {
+        console.log(error);
+    });
+    osandports.startScan();
+
+    return k;
+}
